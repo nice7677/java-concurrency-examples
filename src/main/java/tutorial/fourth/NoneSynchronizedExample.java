91bb0301bda1466845cf5fe00637c682b12dfc47
이진우 <nice7677@naver.com>
2022-02-01 22:28:26 +0800

    - Concurrency issues and Thread Synchronization

---
+++ src/main/java/tutorial/fourth/NoneSynchronizedExample.java
@@ -0,0 +1,45 @@
+package tutorial.fourth;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class NoneSynchronizedExample {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        Counter counter = new Counter();
+
+        for (int i = 0; i < 1000; i++) {
+            executorService.submit(() -> counter.increment());
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(60, TimeUnit.SECONDS);
+
+        System.out.println("Final count is : " + counter.getCount());
+
+        /**
+         * Final count is : 996
+         */
+
+    }
+
+}
+
+class Counter {
+
+    int count = 0;
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
+
---
+++ src/main/java/tutorial/fourth/SynchronizedExample.java
@@ -0,0 +1,46 @@
+package tutorial.fourth;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class SynchronizedExample {
+
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService executorService = Executors.newFixedThreadPool(10);
+
+        SynchronizedCounter synchronizedCounter = new SynchronizedCounter();
+
+        for(int i = 0; i < 1000; i++) {
+            executorService.submit(() -> synchronizedCounter.increment2());
+        }
+
+        executorService.shutdown();
+        executorService.awaitTermination(60, TimeUnit.SECONDS);
+
+        System.out.println("Final count is : " + synchronizedCounter.getCount());
+    }
+
+}
+
+class SynchronizedCounter {
+
+    int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public void increment2() {
+
+        synchronized (this) {
+            count++;
+        }
+
+    }
+
+    public int getCount() {
+        return count;
+    }
+}
+
